c381e884c4660cf0d0ddeb46843481e0098abdbb
Harutann <harutaikou02231127@gmail.com>
2023-10-07 17:42:24 +0900

    test

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from test_model import Person
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
     return 'Response Data'
