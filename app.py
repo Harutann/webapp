11de630b16b3908e2723a7563c4fb3132a27411b
Harutann <harutaikou02231127@gmail.com>
2023-10-07 17:41:19 +0900

    test

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from test_model import Person
+
 app = Flask(__name__)
 @app.route('/')
 def index():
