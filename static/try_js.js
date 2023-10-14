ff762553b3df6e6dd41d9b9a624aef7c14af9e40
Harutann <harutaikou02231127@gmail.com>
2023-10-14 17:24:16 +0900

    test

--- static/try_js.js
+++ static/try_js.js
@@ -1,4 +1,22 @@
 function change_table(){
     table_obj = document.getElementById('table_id');
-    table_obj.style.backgroundColor = "red";
+    if (table_obj.style.backgroundColor === "red") {
+        table_obj.style.backgroundColor = "red";
+    }else{
+        table_obj.style.backgroundColor = "";
+    }
+
+}
+
+function change_by_select() {
+    select_obj = document.getElementById('select_id');
+    table_obj = document.getElementById('table_id');
+    // table_obj.style.backgroundColor = "red";
+    if (select_obj.value === "red") {
+        table_obj.style.backgroundColor = "red";
+    }else if (select_obj.value === "blue"){
+        table_obj.style.backgroundColor = "blue";
+    }else{
+        table_obj.style.backgroundColor = "";
+    }
 }
